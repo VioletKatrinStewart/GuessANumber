7245c3fe6bbaeb07b4b5ea01959c02e8b7312a56
Violet Stewart <vkazoo2@gmail.com>
2021-10-04 14:34:40 -0700

    added ids that I forgot and began js

---
+++ file.js
@@ -0,0 +1,5 @@
+const submitButton = document.getElementById('button');
+const numberInput = document.getElementById('number-input');
+const result = document.getElementById('result');
+
+//let userGuess = 'number-input';
